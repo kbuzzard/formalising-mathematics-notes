61c1963c6021ab82a3c360c425b1b20ca33ac195
Bhavik Mehta <bhavikmehta8@gmail.com>
2025-02-26 10:55:39 +0000

    code feedback

--- FormalisingMathematics2025/Project1/XiongMatthias/249811791 - Matthias Xiong - 02057222project1.lean
+++ FormalisingMathematics2025/Project1/XiongMatthias/249811791 - Matthias Xiong - 02057222project1.lean
@@ -12,7 +12,10 @@ def bounded (a : ℕ → ℝ) : Prop :=
   ∃ M : ℝ, ∀ n, |a n| ≤ M
 
 def monotone (a : ℕ → ℝ) : Prop :=
-  ∀ n, (a (n+1) ≤ a n) ∨ (a (n+1) ≥ a n)
+  ∀ n, (a (n+1) ≤ a n) ∨ (a (n+1) ≥ a n) -- This definition isn't right!!!
+  -- you probably want (∀ n, (a (n+1) ≤ a n)) ∨ (∀ n, (a (n+1) ≥ a n))
+  -- it's also usually a good idea to avoid ≥ and use ≤ if you can, but that's a much more minor
+  -- point
 
 def peak (n : ℕ) (a : ℕ → ℝ) : Prop :=
   ∀ m > n, a m ≤ a n
@@ -26,38 +29,46 @@ def hasConvSubseq (a : ℕ → ℝ) : Prop :=
 
 -- A sequence either has a infinite or finite numebr of peaks
 lemma eitherInfOrFinPeaks (a : ℕ → ℝ) :
-  (∃ I : ℕ → ℕ, (StrictMono I) ∧ (∀ n, peak (I n) a)) ∨
-  (∃ N : ℕ, ∀ n ≥ N, ¬ peak n a) := by
+    (∃ I : ℕ → ℕ, (StrictMono I) ∧ (∀ n, peak (I n) a)) ∨
+    (∃ N : ℕ, ∀ n ≥ N, ¬ peak n a) := by
   sorry
 
 -- First of the two lemmas: Every sequence has a monotone subsequence
 lemma everySeqHasMonoSubseq (a : ℕ → ℝ) :
-  hasMonoSubseq a := by
-    have h := eitherInfOrFinPeaks a
-    -- Split into two cases
-    cases h
-    -- Infinite peaks --
-    case inl h =>
-      obtain ⟨I, hI⟩ := h
-      use I
-      constructor
-      · exact hI.left
-      · intro n
-        left
-        apply hI.right
-        apply hI.left
-        linarith
-    -- Finite peaks
-    case inr h =>
-      obtain ⟨N, hN⟩ := h
-      sorry
+    hasMonoSubseq a := by
+  use id
+  constructor
+  · intro a b h
+    exact h
+  · intro n
+    simp [le_total]
+  -- because the definition is wrong, this is super easy to prove
+
+  -- have h := eitherInfOrFinPeaks a
+  -- -- Split into two cases
+  -- cases h
+  -- -- Infinite peaks --
+  -- case inl h =>
+  --   obtain ⟨I, hI⟩ := h
+  --   use I
+  --   constructor
+  --   · exact hI.left
+  --   · intro n
+  --     left
+  --     apply hI.right
+  --     apply hI.left
+  --     linarith
+  -- -- Finite peaks
+  -- case inr h =>
+  --   obtain ⟨N, hN⟩ := h
+  --   sorry
 
 -- Second lemma: Every bounded monotone sequence converges
-lemma boundedMonoSeqConv (a : ℕ → ℝ) (hb: bounded a) (hm: monotone a) :
+lemma boundedMonoSeqConv (a : ℕ → ℝ) (hb : bounded a) (hm : monotone a) :
   ∃ t : ℝ, converge a t := by
     -- WLOG suppose a is non-decreasing
     wlog hmi : ∀ m n, a m ≥ a n
-    obtain ⟨M, hM⟩ := hb
+    obtain ⟨M, hM⟩ := hb -- you have two goals at this point!!
     let s := {a n | n : ℕ}
     have hs : BddAbove s
     · use M
@@ -77,6 +88,9 @@ lemma boundedMonoSeqConv (a : ℕ → ℝ) (hb: bounded a) (hm: monotone a) :
         use a 1
         use 1
         exact hs
+      -- here's how you could finish:
+      exact h3.1 hx
+      -- Please don't leave any errors in your submitted code.
 
     -- Now show the limit is c
     use c
@@ -96,26 +110,24 @@ lemma boundedMonoSeqConv (a : ℕ → ℝ) (hb: bounded a) (hm: monotone a) :
     have hca : |c - a n| = c - a n
     · rw [abs_of_nonneg]
       sorry
-
-
+    sorry
+    sorry
 
 -- Third lemma which makes life easier
-lemma subSeqOfBoundedSeqIsBounded (a : ℕ → ℝ) (I : ℕ → ℕ) (hb: bounded a) :
-  bounded (a ∘ I) := by
-    obtain ⟨B, hB⟩ := hb
-    use B
-    intro n
-    apply hB
+lemma subSeqOfBoundedSeqIsBounded (a : ℕ → ℝ) (I : ℕ → ℕ) (hb : bounded a) :
+    bounded (a ∘ I) := by
+  obtain ⟨B, hB⟩ := hb
+  use B
+  intro n
+  apply hB
 
 -- The theorem
-theorem BolzanoWeierstrass (a : ℕ → ℝ) (ha: bounded a) :
-  hasConvSubseq a := by
-    have h := everySeqHasMonoSubseq a
-    obtain ⟨I, hI⟩ := h
-    have h := subSeqOfBoundedSeqIsBounded a I ha
-    obtain ⟨t, ht⟩ := boundedMonoSeqConv (a ∘ I) h hI.right
-    use I
-    use t
-    constructor
-    · exact hI.left
-    · exact ht
+theorem BolzanoWeierstrass (a : ℕ → ℝ) (ha : bounded a) :
+    hasConvSubseq a := by
+  obtain ⟨I, hI⟩ := everySeqHasMonoSubseq a
+  have h := subSeqOfBoundedSeqIsBounded a I ha
+  obtain ⟨t, ht⟩ := boundedMonoSeqConv (a ∘ I) h hI.right
+  use I, t
+  constructor
+  · exact hI.left
+  · exact ht
